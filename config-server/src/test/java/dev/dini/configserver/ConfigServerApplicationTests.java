8aaf5c07458c3cd2faed9f97a797487db0d32b85
Tumelo <115115538+tumelosizani@users.noreply.github.com>
2025-01-04 23:59:30 +0200

    first commit

---
+++ config-server/src/test/java/dev/dini/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dini.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
