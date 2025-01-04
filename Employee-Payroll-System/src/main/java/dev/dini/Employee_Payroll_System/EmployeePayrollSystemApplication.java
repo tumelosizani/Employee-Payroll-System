66bb55acba93f6e724930b2174e2c23d1f186faf
Tumelo <115115538+tumelosizani@users.noreply.github.com>
2025-01-04 20:15:43 +0200

    first commit

---
+++ Employee-Payroll-System/src/main/java/dev/dini/Employee_Payroll_System/EmployeePayrollSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.dini.Employee_Payroll_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollSystemApplication.class, args);
+	}
+
+}
---
+++ Employee-Payroll-System/src/test/java/dev/dini/Employee_Payroll_System/EmployeePayrollSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dini.Employee_Payroll_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
