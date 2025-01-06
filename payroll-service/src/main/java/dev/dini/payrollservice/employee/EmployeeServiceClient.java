3984c1fa2e77acf4b7129865d3a5acbb1c39587f
Tumelo <115115538+tumelosizani@users.noreply.github.com>
2025-01-06 15:16:39 +0200

    Update EmployeeServiceClient

--- payroll-service/src/main/java/dev/dini/payrollservice/employee/EmployeeServiceClient.java
+++ payroll-service/src/main/java/dev/dini/payrollservice/employee/EmployeeServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "employee-service", url = "http://employee-service")
+@FeignClient(name = "employee-service", url = "${application.config.employee-service-url}")
 public interface EmployeeServiceClient {
     @GetMapping("/api/v1/employees/{employeeId}")
     EmployeeDTO getEmployeeById(@PathVariable("employeeId") Integer employeeId);
