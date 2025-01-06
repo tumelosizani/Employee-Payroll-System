b7823852361ef7c734d705f47e5c02b24509e339
Tumelo <115115538+tumelosizani@users.noreply.github.com>
2025-01-06 03:00:37 +0200

    first commit

---
+++ tax-service/src/test/java/dev/dini/taxservice/TaxServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dini.taxservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
