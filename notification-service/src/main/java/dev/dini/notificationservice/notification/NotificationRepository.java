3a3d1df45260c45329a48777341d3dea1d5db553
Tumelo <115115538+tumelosizani@users.noreply.github.com>
2025-01-06 15:55:07 +0200

    first commit

---
+++ notification-service/src/main/java/dev/dini/notificationservice/notification/NotificationRepository.java
@@ -0,0 +1,6 @@
+package dev.dini.notificationservice.notification;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+}
---
+++ notification-service/src/main/java/dev/dini/notificationservice/notification/NotificationType.java
@@ -0,0 +1,7 @@
+package dev.dini.notificationservice.notification;
+
+public enum NotificationType {
+    EMAIL,
+    SMS,
+    PUSH_NOTIFICATION
+}
