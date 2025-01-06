b44ffe149ef5ab5c32754ed48444e4b72455ad52
Tumelo <115115538+tumelosizani@users.noreply.github.com>
2025-01-06 17:06:18 +0200

    update

---
+++ Leave Management Service/src/test/java/dev/dini/leavemanagementservice/LeaveManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dini.leavemanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
